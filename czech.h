f83b8260609ef47884dc64ea4893c24432a92d7e
Redak37 <39886519+Redak37@users.noreply.github.com>
2021-11-02 14:51:38 +0100

    doplňuje rody, překlady, unifikuje c/c++ vstupy/výstupy (#26)
    * doplňuje rody, překlady, unifikuje std vstupy/výstupy
    
    Vysvětlivka
    
    Co-authored-by: Ondřej Kinšt <toastercz@gmail.com>

--- czech.h
+++ czech.h
@@ -11,30 +11,40 @@
 // typy
 #define SOUBOR FILE
 #define plav float
+#define plov float
 #define plovoucí float
 #define vznášej float
 #define dvojnásobný double
+#define dvojnásobná double
+#define dvojnásobné double
 #define dvojitý double
+#define dvojitá double
 #define dvojité double
 #define číslo int
+#define cel int // INTeger -> CELé číslo
 #define znak char
-#define dlouhé long
 #define dlouhý long
-#define krátké short
+#define dlouhá long
+#define dlouhé long
 #define krátký short
-#define znaménkové signed
+#define krátká short
+#define krátké short
 #define znaménkový signed
+#define znaménková signed
+#define znaménkové signed
 #define bezznaménkový unsigned
+#define bezznaménková unsigned
 #define bezznaménkové unsigned
-#define neznaménkové unsigned
 #define neznaménkový unsigned
+#define neznaménková unsigned
+#define neznaménkové unsigned
 #define prázdno void
 #define prázdný void
 #define prázdná void
 #define prázdné void
 #define nijaký void
-#define nijaké void
 #define nijaká void
+#define nijaké void
 #define prázdnota void
 #define pravdivost bool
 
@@ -48,6 +58,14 @@
 #define nčíslo16_t uint16_t
 #define nčíslo32_t uint32_t
 #define nčíslo64_t uint64_t
+#define cel8_t int8_t
+#define cel16_t int16_t
+#define cel32_t int32_t
+#define cel64_t int64_t
+#define ncel8_t uint8_t
+#define ncel16_t uint16_t
+#define ncel32_t uint32_t
+#define ncel64_t uint64_t
 
 // logické konstrukce
 #define když if
@@ -59,21 +77,28 @@
 #define zatímco while
 #define pro for
 #define dělej do
+#define udělej do
+#define proveď do
 #define přepínač switch
 #define případ case
 #define výchozí default
 #define rozbij break
+#define rozbít break
+#define přeruš break
+#define přerušit break
 #define pokračuj continue
 
 // ostatní klíčová slova
 #define vrať return
 #define navrať return
-#define stálé static
 #define stálý static
 #define stálá static
-#define neměnné const
+#define stálé static
+#define konst const
+#define konstantní const
 #define neměnný const
 #define neměnná const
+#define neměnné const
 #define jdido goto
 #define velikost(x) sizeof(x)
 
@@ -81,10 +106,16 @@
 
 // operátory
 #define je ==
+#define rovný ==
 #define rovná ==
+#define rovné ==
+#define rovno ==
 #define rovnáse ==
 #define není !=
+#define nerovný !=
 #define nerovná !=
+#define nerovné !=
+#define nerovno !=
 #define nerovnáse !=
 
 // experimentální triky
@@ -110,9 +141,19 @@
 #define DVOJKOVÝ_PŘÍPIS "wa"
 
 // standardní I/O streamy
+#define standovnitř stdin
 #define stanvstup stdin
+#define stando stdin
 #define stanvýstup stdout
+#define stanven stdout
 #define stanchyb stderr
+
+#define stídovnitř stdin
+#define stívstup stdin
+#define stído stdin
+#define stívýstup stdout
+#define stíven stdout
+#define stíchyb stderr
 
 // konstanty
 #define pravda 1
@@ -154,16 +195,32 @@
 */
 // c -> character -> znak -> z
 #define zven cout
-#define cven cout
+#define zvýstup cout
 #define zdovnitř cin
-#define cdovnitř cin
+#define zvstup cin
 #define zdo cin
+#define zerr cerr
+#define zchyb cerr
+
+#define cven cout
+#define cvýstup cout
+#define cdovnitř cin
+#define cvstup cin
 #define cdo cin
+#define cchyb cerr
 
 #define používá using
 #define jménovesmír namespace
+#define jmennýprostor namespace
 // std -> sexually transmitted disease -> Sexuálně přenosná nemoc -> spn
 #define spn std
+// STandarD -> STandardnÍ
+#define stí std
 
 #define vektor vector
 #define zatlač_zpátky push_back
+#define zatlač_zpět push_back
+#define zatlač_dozadu push_back
+#define strč_zpátky push_back
+#define strč_zpět push_back
+#define strč_dozadu push_back
